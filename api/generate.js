f05a475e2ab9bf8d6c645c02c0d4ffa426324bfb
Elias Rhouzlane <elias.rhouzlane@gmail.com>
2022-06-06 16:40:58 +0200

    feat: add cache control

--- api/generate.js
+++ api/generate.js
@@ -2,7 +2,10 @@ import QRCode from "qrcode";
 
 export default ({ query: { text, dark, light, width, scale, margin } }, res) =>
   QRCode.toFileStream(
-    res.writeHead(200, { "Content-Type": "image/png" }),
+    res.writeHead(200, {
+      "Content-Type": "image/png",
+      "Cache-Control": `s-maxage=${86400 * 30}, stale-while-revalidate`,
+    }),
     text,
     {
       width,
